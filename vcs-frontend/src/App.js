c5146bff4be29475cd6a3eb2b71484dfafa64348
Shreya-bansall <shreyab2400@gmail.com>
2023-12-07 13:17:54 +0530

    Shreya component added

--- vcs-frontend/src/App.js
+++ vcs-frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import shreya from './components/shreya'
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <shreya />
+
     </div>
   );
 }
---
+++ vcs-frontend/src/components/shreya.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function shreya() {
+  return (
+    <div>
+      <h2>Hello, I am Shreya</h2>
+    </div>
+  );
+}
+
+export default HelloComponent;
